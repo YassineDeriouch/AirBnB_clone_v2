f1322748cc2a38d93904254773fe40b7e88cc4bf
YassineDeriouch <yderouich14@gmail.com>
2023-09-17 08:05:40 +0000

    setup_mysql_test.sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+ -- SQL script to test MySQL server for the project
+
+-- create hbnb_test_db database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- create hbnb_test user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- grant all privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- grant SELECT statement privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- flush privileges to apply the changes
+FLUSH PRIVILEGES;
