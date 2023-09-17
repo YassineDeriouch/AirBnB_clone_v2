17fdcfc1f157593cda6d48ff40ac423c4bb28ec1
YassineDeriouch <yderouich14@gmail.com>
2023-09-17 10:59:48 +0000

    upd city.py

--- models/city.py
+++ models/city.py
@@ -1,16 +1,15 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
 class City(BaseModel, Base):
     """
-    This class defines a City by its name.
+    This class defines a City.
         __tablename__ (str): The table name for the database table.
         name (str): The name of the City.
         state_id (str): The State ID associated with the City.
     """
     __tablename__ = 'cities'
-   if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
