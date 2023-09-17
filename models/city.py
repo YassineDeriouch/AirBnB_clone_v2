a48fe20d68798c8dc6fdfb5b5263ebe642d7dfae
YassineDeriouch <yderouich14@gmail.com>
2023-09-17 10:55:19 +0000

    upd city.py

--- models/city.py
+++ models/city.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
@@ -10,7 +10,7 @@ class City(BaseModel, Base):
         name (str): The name of the City.
         state_id (str): The State ID associated with the City.
     """
-
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False
+   if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
