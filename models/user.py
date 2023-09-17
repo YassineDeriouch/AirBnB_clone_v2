0c7856a04de86ac50a46d0703100ed34c46edfd2
YassineDeriouch <yderouich14@gmail.com>
2023-09-17 16:23:57 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -4,15 +4,14 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
+
 class User(BaseModel, Base):
-     """
-        This class defines a user
-
-        email      : user's email
-        password   : user's password
-        first_name : user's first name
-        last_name  : user's last name
-
+    """
+    This class defines a user
+    email : user's email
+    password : user's password
+    first_name : user's first name
+    last_name : user's last name
     """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
