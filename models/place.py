a0eeb3f170ede2d7132029017c41d2ab310141c4
YassineDeriouch <yderouich14@gmail.com>
2023-09-17 11:32:46 +0000

    9. user place review

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Place """
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
