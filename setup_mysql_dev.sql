8b3830ea920baefd262f5b6ec810c831a6b8b406
YassineDeriouch <yderouich14@gmail.com>
2023-09-17 07:31:38 +0000

    upd setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,12 @@
 -- SQL script to prepares MySQL server for the project
 
--- Create the database if it doesn't exist
+-- create database if it not exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Create the user if it doesn't exist
+-- create user if it not exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Grant all privileges on hbnb_dev_db to hbnb_dev
+-- grant all privileges on hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- Grant SELECT privilege on performance_schema to hbnb_dev
+-- grant SELECT statement privilege on performance_schema to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- Flush privileges to apply the changes
+-- flush privileges to apply theses changes above
 FLUSH PRIVILEGES;
