bfaf6e21e13826354579f4993504fb834dda4d78
YassineDeriouch <42626724+YassineDeriouch@users.noreply.github.com>
2023-09-17 10:03:08 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -2,9 +2,6 @@
 """Defines the User class."""
 from models.base_model import Base
 from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -24,5 +21,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
